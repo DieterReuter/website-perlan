ae3f094cca86423e85b7f60ed0f0956932ce2bdf
Dieter Reuter <dieter.reuter@me.com>
2016-04-10 20:21:54 +0200

    Add auto TLS feature for prod server

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-(cd website && caddy -agree=true -conf=../Caddyfile -log=stderr)
+if [ "$(uname -s)" == "Darwin" ]; then
+  # dev machine
+  CADDYFILE="Caddyfile"
+else
+  # prod machine
+  CADDYFILE="Caddyfile.tls"
+fi
+(cd website && caddy -agree=true -email="dieter.reuter@me.com" -conf=../${CADDYFILE} -log=stderr)

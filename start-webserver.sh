ccc7b3b978edb28dfa1f29e4bc9d899440f0bc1b
Dieter Reuter <dieter.reuter@me.com>
2016-04-10 20:48:04 +0200

    Download latest Caddy version

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+CADDY_VERSION="v0.8.2"
 
 if [ "$(uname -s)" == "Darwin" ]; then
-  # dev machine
-  CADDYFILE="Caddyfile"
+  # dev machine: OS X
+  CADDY_FILE="Caddyfile"
+
+  # download Caddy binary
+  if [ ! -f "caddy_darwin_amd64.zip" ]; then
+    wget https://github.com/mholt/caddy/releases/download/${CADDY_VERSION}/caddy_darwin_amd64.zip
+  fi
 else
-  # prod machine
-  CADDYFILE="Caddyfile.tls"
+  # prod machine: Linux ARMv7, Scaleway C1
+  CADDY_FILE="Caddyfile.tls"
+
+  # download Caddy binary
+  if [ ! -f "caddy_linux_arm.tar.gz" ]; then
+    wget https://github.com/mholt/caddy/releases/download/${CADDY_VERSION}/caddy_linux_arm.tar.gz
+  fi
 fi
-(cd website && caddy -agree=true -email="dieter.reuter@me.com" -conf=../${CADDYFILE} -log=stderr)
+
+# start Caddy webserver
+(cd website && caddy -agree=true -email="dieter.reuter@me.com" -conf=../${CADDY_FILE} -log=stderr)
